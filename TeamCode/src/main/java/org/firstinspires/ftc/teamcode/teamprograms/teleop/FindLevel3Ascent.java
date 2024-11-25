2dcccc9f6cbc807c33280a1bf1fcea8ca7dee4bc
Connor W <cwiebesick@westlutheran.net>
2024-11-24 19:08:45 -0600

    update to level 3 ascent test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamprograms/teleop/FindLevel3Ascent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamprograms/teleop/FindLevel3Ascent.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.teamprograms.teleop;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -7,19 +8,30 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(group = "Z")
+@Config
 public class FindLevel3Ascent extends LinearOpMode {
 
     DcMotorEx linearSlideLift, linearSlidePivot;
     DcMotorEx linearActuatorRight, linearActuatorLeft;
+    DcMotorEx frontRight, backRight, frontLeft, backLeft;
 
     private final double SLIDE_EXTENSION_MAX = 4200;
     private final double CUSHION_RATIO = 400;
-    private final double GRAVITY_COEFFICIENT = 0.0005;
+//    private final double GRAVITY_COEFFICIENT = 0.0005;
+
+    public static class gravity {
+        public double coefficient = 0.0005;
+    }
+    public static gravity GRAVITY = new gravity();
 
 
-    private final double ACTUATOR_SPEED = 0.5;
+    private final double ACTUATOR_SPEED = 1.0;
     private final double SLIDE_SPEED = 0.7;
-    private final double PIVOT_SPEED = 0.9;
+    private final double PIVOT_SPEED = 0.4;
+
+    private final double DRIVE_SPEED = 0.6;
+
+    private boolean enableGravity = true;
 
 
     @Override
@@ -44,6 +56,19 @@ public class FindLevel3Ascent extends LinearOpMode {
         linearActuatorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         linearActuatorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+//        linearActuatorRight.setDirection(DcMotorSimple.Direction.REVERSE);
+//        linearActuatorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        boolean checkGTwoA = true;
+
 
         telemetry.addLine("Press Start");
         telemetry.update();
@@ -51,6 +76,14 @@ public class FindLevel3Ascent extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
+
+            if (!gamepad2.a) checkGTwoA = false;
+
+            // enable/disable gravity
+            if (gamepad2.a && !checkGTwoA) {
+                checkGTwoA = true;
+                enableGravity = !enableGravity;
+            }
 
             // get positions
             double linearSlidePosition = linearSlideLift.getCurrentPosition();
@@ -60,6 +93,7 @@ public class FindLevel3Ascent extends LinearOpMode {
             double powerActuatorUp = (-gamepad2.right_trigger) * ACTUATOR_SPEED;
             double powerActuatorDown = (-gamepad2.left_trigger) * ACTUATOR_SPEED;
             linearActuatorRight.setPower(powerActuatorUp - powerActuatorDown);
+            linearActuatorLeft.setPower(powerActuatorUp - powerActuatorDown);
 
             // move linear slide
             double powerSlide = calculateSlidePower(linearSlidePosition);
@@ -71,6 +105,27 @@ public class FindLevel3Ascent extends LinearOpMode {
 
 
 
+            // tank mecanum drive
+
+            double rightPower = -gamepad1.right_stick_y * DRIVE_SPEED;
+            double leftPower = -gamepad1.left_stick_y * DRIVE_SPEED;
+
+            double slideToRight = -gamepad1.right_trigger * DRIVE_SPEED * 1.1;
+            double slideToLeft = -gamepad1.left_trigger * DRIVE_SPEED * 1.1;
+
+            double tankDenominator = Math.max(Math.abs((rightPower+leftPower)/2) + Math.abs(slideToRight) + Math.abs(slideToLeft), 1);
+
+            frontRight.setPower((rightPower + slideToRight - slideToLeft) / tankDenominator);
+            backRight.setPower((rightPower - slideToRight + slideToLeft) / tankDenominator);
+            frontLeft.setPower((leftPower - slideToRight + slideToLeft)/ tankDenominator);
+            backLeft.setPower((leftPower + slideToRight - slideToLeft)/ tankDenominator);
+
+
+
+            telemetry.addData("GRAVITY COEFFICIENT ENABLED", enableGravity);
+            telemetry.addLine("G2 button_a enables/disables gravity");
+            telemetry.addLine("NOTE if gravity is on all the time, the motor will burn");
+            telemetry.addLine("--------------------------------");
             telemetry.addLine("CONTROLS (G2)");
             telemetry.addLine("L/R triggers: linear actuators");
             telemetry.addLine("Right stick: linear slide");
@@ -97,23 +152,25 @@ public class FindLevel3Ascent extends LinearOpMode {
         double linearSlideCushion = (SLIDE_EXTENSION_MAX - linearSlidePosition)
                 /CUSHION_RATIO;
 
-        // determine whether to apply the cushion or ignore it
+        // use the power cushion if robot is extending, ignore if retracting
         if ((-gamepad2.right_stick_y) > 0) {
-            // lift going up, use cushion
-            linearSlidePower = (-gamepad2.right_stick_y)*SLIDE_SPEED*linearSlideCushion;
+            linearSlidePower = (((-gamepad2.right_stick_y)*SLIDE_SPEED*0.75)*
+                    linearSlideCushion);
         } else {
-            // lift going down, ignore cushion
-            linearSlidePower = (-gamepad2.right_stick_y)*SLIDE_SPEED;
+            // make lowering slides slower
+            linearSlidePower = ((-gamepad2.right_stick_y)*SLIDE_SPEED*0.5);
         }
 
         // make descent of slides slightly slower so it is not jarring
-        if ((-gamepad2.right_stick_y) < 0) {
+        /*if ((-gamepad2.right_stick_y) < 0) {
             linearSlidePower*=0.9;
-        }
+        }*/
 
 
         // apply a coefficient to fight gravity
-        linearSlidePower+=GRAVITY_COEFFICIENT;
+        if (enableGravity) {
+            linearSlidePower-=GRAVITY.coefficient;
+        }
 
         return linearSlidePower;
     }
